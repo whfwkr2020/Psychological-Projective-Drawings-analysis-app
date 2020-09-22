ccb43edd4dc7e550b60654590cf3f9b658b1a3d3
Junho Kim <57608392+KimJunho97@users.noreply.github.com>
2020-09-22 21:20:37 +0900

    Canvas 수정

--- Team2020/app/src/main/java/com/example/team2020/MyPage.java
+++ Team2020/app/src/main/java/com/example/team2020/MyPage.java
@@ -1,26 +1,31 @@
 package com.example.team2020;
 
+import android.app.Fragment;
+import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.app.FragmentManager;
 
 public class MyPage extends AppCompatActivity {
 
-    Button menu, user, test, journal, infochange;
+    Button menu, user, test, journal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mypage_layout);
 
+        MyPageFragment1 fragment1 = new MyPageFragment1();
+        changeFragment(fragment1);
+
         menu = findViewById(R.id.menu);
         user = findViewById(R.id.my);
         test = findViewById(R.id.testrecord);
         journal = findViewById(R.id.journalrecord);
-        infochange = findViewById(R.id.infochange12);
 
 
         menu.setOnClickListener(new View.OnClickListener() {
@@ -34,33 +39,34 @@ public class MyPage extends AppCompatActivity {
         user.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent11 = new Intent(v.getContext(), MyPage.class);
-                startActivity(intent11);
+                MyPageFragment1 fragment1 = new MyPageFragment1();
+                changeFragment(fragment1);
             }
         }); //user 버튼 선택시
 
         test.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent12 = new Intent(v.getContext(), MyPage2.class);
-                startActivity(intent12);
+                MyPageFragment2 fragment2 = new MyPageFragment2();
+                changeFragment(fragment2);
             }
         }); //test 버튼 선택시
 
         journal.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent13 = new Intent(v.getContext(), MyPage3.class);
-                startActivity(intent13);
+                MyPageFragment3 fragment3 = new MyPageFragment3();
+                changeFragment(fragment3);
             }
         }); //journal 버튼 선택시
 
-        infochange.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent10 = new Intent(v.getContext(), MyPage12.class);
-                startActivity(intent10);
-            }
-        }); //infochange 버튼 선택시
+    }
+
+    public void changeFragment(Fragment fragment)
+    {
+       FragmentManager fragmentManager = getFragmentManager();
+       FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_container, fragment);
+        fragmentTransaction.commit();
     }
 }
